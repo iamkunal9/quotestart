953f586c00b51b704edb23e709f34cb5fce02bd2
iamkunal9 <89696700+iamkunal9@users.noreply.github.com>
2021-08-28 20:07:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// Powered by Quotable
+// https://github.com/lukePeavey/quotable
+
+document.addEventListener("DOMContentLoaded", () => {
+  // DOM elements
+  const quote = document.querySelector("blockquote p");
+  const cite = document.querySelector("blockquote cite");
+
+  async function updateQuote() {
+    // Fetch a random quote from the Quotable API
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    if (response.ok) {
+      // Update DOM elements
+      quote.textContent = data.content;
+      cite.textContent = data.author;
+    } else {
+      quote.textContent = "An error occured";
+      console.log(data);
+    }
+  }
+
+
+  // call updateQuote once when page loads
+  updateQuote();
+});
